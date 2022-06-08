ef7a7076b77eeaa11ce468d4d8d13e808cee9df7
kenlol <k080191k@gmail.com>
2022-06-08 17:48:42 +0800

    Increased Size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EtaComponent } from './eta/eta.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { EtaContainerComponent } from './eta-container/eta-container.component';
@@ -16,7 +15,6 @@ import { AutocompleteLibModule } from 'angular-ng-autocomplete';
     AppComponent,
     EtaComponent,
     HeaderComponent,
-    FooterComponent,
     EtaContainerComponent,
   ],
   imports: [
